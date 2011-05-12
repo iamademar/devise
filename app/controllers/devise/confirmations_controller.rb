5c50a26e1e7f774bb02c1ea60855a6ad4edeaabb
Ademar <ademar@ademar-laptop.(none)>
2011-05-12 09:47:41 +0800

    confirm controllers

--- app/controllers/devise/confirmations_controller.rb
+++ app/controllers/devise/confirmations_controller.rb
@@ -26,7 +26,9 @@ class Devise::ConfirmationsController < ApplicationController
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_navigational_format?
       sign_in(resource_name, resource)
-      respond_with_navigational(resource){ redirect_to redirect_location(resource_name, resource) }
+      profile = Igniter::Profile.find(:first, :conditions => { :user_id => resouce.id })
+      redirect_to edit_igniter_profile_path(profile)
+      # respond_with_navigational(resource){ redirect_to redirect_location(resource_name, resource) }
     else
       respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render_with_scope :new }
     end
